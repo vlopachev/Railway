0072943f41cc319428fffb9ba2729855bf61f239
Vyacheslav Lopachev <vmlopach@gmail.com>
2018-11-09 22:39:45 +0600

    add logic

--- src/com/epam/railway/logic/RailwayUtils.java
+++ src/com/epam/railway/logic/RailwayUtils.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public final class RailwayUtils {
     public final static double WEIGHT_AVERAGE_PERSON = 0.062; //ton
-    public final static double WEIGHT_LITER_SAND = 0.0017;
+
     // Locomotives
     public final static int    POWER_2ES4K = 5920; //kW
     public final static int    POWER_3ES4K = 8880;
@@ -17,22 +17,18 @@ public final class RailwayUtils {
     public final static double SAND_STOCK_3ES4K = 3;
     // Covered wagon
     public final static double WEIGHT_COVERED_WAGON = 26.0;
-    public final static double VOLUME_COVERED_WAGON = 138.0; // CBM
     public final static double PAYLOAD_COVERED_WAGON = 68.0;
     // Open wagon
     public final static double WEIGHT_OPEN_WAGON = 23.0;
-    public final static double VOLUME_OPEN_WAGON = 75.2;
     public final static double PAYLOAD_OPEN_WAGON = 71.0;
     // Platform wagon
     public final static double WEIGHT_PLATFORM_WAGON = 23.5;
     public final static double PAYLOAD_PLATFORM_WAGON = 71.0;
     // Tank wagon
     public final static double WEIGHT_TANK_WAGON = 27.0;
-    public final static double VOLUME_TANK_WAGON = 83.0;
     public final static double PAYLOAD_TANK_WAGON = 67.0;
     // Hopper wagon
     public final static double WEIGHT_HOPPER_WAGON = 23.0;
-    public final static double VOLUME_HOPPER_WAGON = 81.0;
     public final static double PAYLOAD_HOPPER_WAGON = 70.0;
     // Post wagon
     public final static double WEIGHT_POST_WAGON = 46.7;
